657b56b1ee46c6b80663972e09cb76a4b401ea1a
mvykool <maykolandres4@gmail.com>
2024-03-28 21:36:34 -0500

    cleaned some spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,10 +64,8 @@ const Header = () => {
               </a>
             ))}
           </div>
-
           <HamburgerMenu />
         </nav>
-
         <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
